233ed52b246b988d0a85841a53ae88ef575d4d7a
pcplws <shrutiravi@rediffmail.com>
2020-01-30 14:07:15 +0530

    Fifth Commit: Change in mySum.R

--- R/mySum.R
+++ R/mySum.R
@@ -1,4 +1,14 @@
+#' mySum
+#'
+#' Basic R function to be called from the demo app
+#'
+#' @export
+#' @param input_1 Number One. Required.
+#' @param input_2 Number Two. Required.
+
 mySum <- function(input_1 = 10, input_2 = 20) {
-  output <- input_1 + input_2
-  return(output)
+  myoutput <- input_1 + input_2
+  list(
+    message = paste("The Sum is : ", myoutput, "! This is", R.Version()$version.string)
+  )
 }
